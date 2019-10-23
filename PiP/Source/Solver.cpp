e2fbf62fa3bfd4bf5676b42e0c837d5ab6eadc39
Javier Dieguez <30554337+JavierDega@users.noreply.github.com>
2019-10-23 14:07:19 +0100

    response

--- PiP/Source/Solver.cpp
+++ PiP/Source/Solver.cpp
@@ -117,7 +117,33 @@ decimal Solver::ComputeSweep(Rigidbody * rb1, Rigidbody * rb2, decimal dt)
 
 void Solver::ComputeResponse(Rigidbody * rb1, Rigidbody * rb2)
 {
+	/*// First, find the normalized vector n from the center of
+// circle1 to the center of circle2
+Vector n = circle1.center - circle2.center;
+n.normalize();
+// Find the length of the component of each of the movement
+// vectors along n.
+// a1 = v1 . n
+// a2 = v2 . n
+float a1 = v1.dot(n);
+float a2 = v2.dot(n);
 
+// Using the optimized version,
+// optimizedP =  2(a1 - a2)
+//              -----------
+//                m1 + m2
+float optimizedP = (2.0 * (a1 - a2)) / (circle1.mass + circle2.mass);
+
+// Calculate v1', the new movement vector of circle1
+// v1' = v1 - optimizedP * m2 * n
+Vector v1' = v1 - optimizedP * circle2.mass * n;
+
+// Calculate v1', the new movement vector of circle1
+// v2' = v2 + optimizedP * m1 * n
+Vector v2' = v2 + optimizedP * circle1.mass * n;
+
+circle1.setMovementVector(v1');
+circle2.setMovementVector(v2');*/
 }
 Rigidbody * Solver::AddBody()
 {
