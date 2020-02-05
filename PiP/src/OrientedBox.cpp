f1d65cc73630dace18eb5ecb48d998a4bce65db2
JavierDega <javierillo20122012@hotmail.com>
2020-02-05 09:14:23 +0000

    WIP SAT Test

--- PiP/src/OrientedBox.cpp
+++ PiP/src/OrientedBox.cpp
@@ -36,16 +36,9 @@ bool OrientedBox::IntersectWith(OrientedBox* rb2, math::Manifold& manifold)
 {
 	//SAT
 	//We only have 4 axis to project to, but we can simplify it by bringing things to one Obb's reference frame
-
-	Vector2 axis = Vector2(1, 0).Rotate(m_rotation);
-
-	decimal projExtents[2];//max and min along axis
-	projExtents[0] = m_position.Dot(axis) + m_halfExtents.x;
-	projExtents[1] = m_position.Dot(axis) - m_halfExtents.x;
-
-	decimal projExtents2[2];
-	//..Dot projections
-
+	if (!TestAxis(Vector2(1, 0).Rotate(m_rotation), m_position, rb2->m_position, m_halfExtents.Rotate(m_rotation),
+		rb2->m_halfExtents.Rotate(rb2->m_rotation))) return false;
+	
 	return false;
 }
 
@@ -67,4 +60,42 @@ decimal OrientedBox::SweepWith(Capsule* rb2, decimal dt, math::Manifold& manifol
 decimal OrientedBox::SweepWith(OrientedBox* rb2, decimal dt, math::Manifold& manifold)
 {
 	return decimal();
+}
+
+bool OrientedBox::TestAxis(math::Vector2 axis, math::Vector2 pos1, math::Vector2 pos2, math::Vector2 rotExtents, math::Vector2 rotExtents2)
+{
+	decimal pos1Axis = pos1.Dot(axis);
+	decimal pos2Axis = pos2.Dot(axis);
+	Vector2 p[4]{ Vector2(rotExtents), Vector2(-rotExtents.x, rotExtents.y), Vector2(-rotExtents), Vector2(rotExtents.x, -rotExtents.y) };
+	Vector2 p2[4]{ Vector2(rotExtents2), Vector2(-rotExtents2.x, rotExtents2.y), Vector2(-rotExtents2), Vector2(rotExtents2.x, -rotExtents2.y) };
+	decimal min = 0, max = 0;
+	decimal min2 = 0, max2 = 0;
+
+	for (int i = 0; i < 4; i++) {
+		decimal projection = p[i].Dot(axis);
+		decimal projection2 = p2[i].Dot(axis);
+		if (projection < min) min = projection;
+		if (projection > max) max = projection;
+		if (projection2 < min2) min2 = projection2;
+		if (projection2 > max2) max2 = projection2;
+	}
+
+	if (pos1Axis <= pos2Axis)
+	{
+		if (pos1Axis + max < pos2Axis - min2)
+		{
+			//Separating axis
+			return false;
+		}
+		else return true;
+	}
+	else 
+	{
+		if (pos2Axis + max2 < pos1Axis - min)
+		{
+			//Separating axis
+			return false;
+		}
+		else return true;
+	}
 }
--- PiP/src/OrientedBox.h
+++ PiP/src/OrientedBox.h
@@ -17,5 +17,8 @@ public:
 	virtual decimal SweepWith(Capsule* rb2, decimal dt, math::Manifold& manifold) override;
 	virtual decimal SweepWith(OrientedBox* rb2, decimal dt, math::Manifold& manifold) override;
 
+	//private
+	bool TestAxis(math::Vector2 axis, math::Vector2 pos1, math::Vector2 pos2, math::Vector2 rotExtents, math::Vector2 rotExtents2);
+
 	math::Vector2 m_halfExtents;
 };
