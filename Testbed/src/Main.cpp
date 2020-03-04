9b2c8d9d0d7083f97c4bb4e3c109dc033ae26031
Javier Dieguez <JDieguez@climax.local>
2020-03-04 14:18:54 +0000

    Added catch2.org for c++ unit testing

--- Testbed/src/Main.cpp
+++ Testbed/src/Main.cpp
@@ -1,9 +1,10 @@
-
+#include "catch.h"
 #include "TestApp.h"
 
 int main(void)
 {
 	//Do tests here (asserts)
+	
 	TestApp testApp;
 	if (testApp.Init() == -1) return -1;//Init error
 	/* Loop until the user closes the window */
